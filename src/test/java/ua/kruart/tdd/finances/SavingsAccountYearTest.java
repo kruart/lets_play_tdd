db8f73d87effd37c9041695ce231a4dbd82cfacd
weoz <weoz@ukr.net>
2016-12-10 11:51:51 +0200

    9. Seeking Beauty

--- src/test/java/ua/kruart/tdd/finances/SavingsAccountYearTest.java
+++ src/test/java/ua/kruart/tdd/finances/SavingsAccountYearTest.java
@@ -1,5 +1,6 @@
 package ua.kruart.tdd.finances;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -46,11 +47,42 @@ public class SavingsAccountYearTest {
     }
 
     @Test
+    public void interestEarnedIsStartingBalanceCombinedWithInterestRate() {
+        SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
+        assertEquals(1000, year.interestEarned(25));
+    }
+
+    @Test
+    public void withdrawnFundsDoNotEarnInterest() {
+        SavingsAccountYear year = newAccount();
+        year.withdraw(1000);
+        assertEquals(900, year.interestEarned(25));
+    }
+
+    @Test
+    public void totalWithdrawnIncludingCapitalGains() {
+        SavingsAccountYear year = new SavingsAccountYear(10000, 0, 10);
+        year.withdraw(1000);
+        assertEquals("capital gains tax", 333, year.capitalGainsTaxIncurred(25));
+        assertEquals("total withdrawn", 1333, year.totalWithdrawn(25));
+    }
+
+    @Test
+    public void capitalGainsTaxesDoNotEarnInterest() {
+        SavingsAccountYear year = new SavingsAccountYear(10000, 0, 10);
+        year.withdraw(1000);
+        assertEquals("capital gains withdrawn", 1000, year.capitalGainsWithdrawn());
+        assertEquals("capital gains tax", 333, year.capitalGainsTaxIncurred(25));
+//        assertEquals("total withdrawn", 1333, year.totalWithdrawnIncludingCapitalGains(25));
+        assertEquals("interest earned", 866, year.interestEarned(25));
+    }
+
+    @Test
+    @Ignore
     public void endingCapitalGainsIncludesInterestEarned() {
         SavingsAccountYear year = new SavingsAccountYear(10000, 3000, 10);
         assertEquals(7000, year.startingCapitalGains());
-
-//        assertEquals(4000, year.endingCapitalGains());
+        assertEquals(4000, year.endingCapitalGains());
     }
 
     @Test
@@ -59,18 +91,11 @@ public class SavingsAccountYearTest {
     }
 
     @Test
-    public void withdrawnFundsDoNotEarnInterest() {
-        SavingsAccountYear year = newAccount();
-        year.withdraw(1000);
-        assertEquals(9900, year.endingBalance(25));
-    }
-
-    @Test
     public void multipleWithdrawalsInYearAreTotaled() {
         SavingsAccountYear year = newAccount();
         year.withdraw(1000);
         year.withdraw(2000);
-        assertEquals(3000, year.totalWithdrawn());
+        assertEquals(3000, year.totalWithdrawnExceptCapitalGainsTax());
     }
 
     @Test
