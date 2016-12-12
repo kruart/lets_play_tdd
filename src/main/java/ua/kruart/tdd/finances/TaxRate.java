cad77d0d86a9bae0e6826919ed74986bc035e6f9
weoz <weoz@ukr.net>
2016-12-12 11:27:53 +0200

    14. Obsessing Over Primitives

--- src/main/java/ua/kruart/tdd/finances/TaxRate.java
+++ src/main/java/ua/kruart/tdd/finances/TaxRate.java
@@ -1,21 +1,46 @@
 package ua.kruart.tdd.finances;
 
-import java.math.BigDecimal;
-
 /**
  * Created by Arthur on 11.12.2016.
  */
 public class TaxRate {
 
-
-    private BigDecimal rate;
+    private double rate;
 
     public TaxRate(double rateAsPercentage) {
-        this.rate = new BigDecimal(rateAsPercentage).divide(new BigDecimal(100));
+        this.rate = rateAsPercentage / 100.0;
     }
 
-    public int taxFor(int amount) {
-//        return (int)(amount * rate);
-        return 0;
+    public int rate() {
+        return (int)(rate * 100);   // TODO: DELETE ME
+    }
+
+    public int simpleTaxFor(int amount) {
+        return (int)(rate * amount);
+    }
+
+    public int compoundTaxFor(int amount) {
+        return (int)((amount / (1 - rate)) - amount);
+    }
+
+    @Override
+    public String toString() {
+        return (rate * 100)+ "%";
+    }
+
+    @Override
+    public int hashCode() {
+        long temp = Double.doubleToLongBits(rate);
+        return (int) (temp ^ (temp >>> 32));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TaxRate taxRate = (TaxRate) o;
+
+        return Double.compare(taxRate.rate, rate) == 0;
     }
 }
--- src/test/java/ua/kruart/tdd/finances/TaxRateTest.java
+++ src/test/java/ua/kruart/tdd/finances/TaxRateTest.java
@@ -3,6 +3,8 @@ package ua.kruart.tdd.finances;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Arthur on 11.12.2016.
@@ -12,12 +14,32 @@ public class TaxRateTest {
     @Test
     public void nothing() {
         TaxRate taxRate = new TaxRate(0);
-        assertEquals(0, taxRate.taxFor(1000));
+        assertEquals(0, taxRate.simpleTaxFor(1000));
+        assertEquals(0, taxRate.compoundTaxFor(1000));
     }
 
     @Test
-    public void simpleTax() {
-        TaxRate taxRate = new TaxRate(0);
-        assertEquals(250, taxRate.taxFor(1000));
+    public void simpleTaxJustAppliesTaxRateToAmount() {
+        TaxRate taxRate = new TaxRate(25);
+        assertEquals(250, taxRate.simpleTaxFor(1000));
     }
+
+    @Test
+    public void compoundTaxIsTheAmountOfTaxThatIsIncurredIfYouAlsoPayTaxOnTheTax() {
+        TaxRate taxRate = new TaxRate(25);
+        assertEquals(333, taxRate.compoundTaxFor(1000));
+    }
+
+    @Test
+    public void valueObject() {
+        TaxRate rate1a = new TaxRate(33);
+        TaxRate rate2b = new TaxRate(33);
+        TaxRate rate2 = new TaxRate(40);
+
+        assertEquals("33.0%", rate1a.toString());
+        assertTrue("same values should be equal", rate1a.equals(rate2b));
+        assertFalse("different values should be false", rate1a.equals(rate2));
+        assertTrue("same values have same hash code", rate1a.hashCode() == rate2b.hashCode());
+    }
+
 }
