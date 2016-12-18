91d9134e368a42c42ac0197a6898583dcf58a6a3
weoz <weoz@ukr.net>
2016-12-18 17:20:55 +0200

    25. Extracting the StockMarket Class

---
+++ src/main/java/ua/kruart/tdd/finances/StockMarket.java
@@ -0,0 +1,36 @@
+package ua.kruart.tdd.finances;
+
+/**
+ * Created by Arthur on 18.12.2016.
+ */
+public class StockMarket {
+
+    private final int startingYear;
+    private final int endingYear;
+    private final Dollars startingBalance;
+    private final Dollars startingPrincipal;
+    private final InterestRate interestRate;
+    private final TaxRate capitalGainsTaxRate;
+
+    public StockMarket(int startingYear, int endingYear, Dollars startingBalance, Dollars startingPrincipal, InterestRate interestRate, TaxRate capitalGainsTaxRate) {
+        this.startingYear = startingYear;
+        this.endingYear = endingYear;
+        this.startingBalance = startingBalance;
+        this.startingPrincipal = startingPrincipal;
+        this.interestRate = interestRate;
+        this.capitalGainsTaxRate = capitalGainsTaxRate;
+    }
+
+    public StockMarketYear getYear(int offset) {
+        StockMarketYear year = new StockMarketYear(startingBalance, startingPrincipal, interestRate, capitalGainsTaxRate);
+        for (int i = 0; i < offset; i++) {
+            year = year.nextYear();
+        }
+
+        return year;
+    }
+
+    public int numberOfYears() {
+        return endingYear - startingYear + 1;
+    }
+}
